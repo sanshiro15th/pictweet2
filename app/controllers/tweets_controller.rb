f6f2150f339ab8c8a9869e2a2873e2d4d05cd4e8
sanshiro15th <sanshiro15th@gmail.com>
2017-01-25 21:02:12 +0900

    tweet機能実装

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,21 @@
+class TweetsController < ApplicationController
+
+  def index
+    @tweets = Tweet.all.order("id DESC").limit(5)
+  end
+
+  def new
+
+  end
+
+  def create
+    Tweet.create(tweet_params)
+  end
+
+  private
+
+  def tweet_params
+    params.permit(:name, :image, :text)
+  end
+
+end
